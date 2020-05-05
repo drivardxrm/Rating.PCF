72d56f38df0b425bb3f28ecfbc2eac523733b53b
drivardxrm <38399134+drivardxrm@users.noreply.github.com>
2020-05-05 07:48:50 -0400

    can have 0 value

--- Rating/RatingControl.tsx
+++ Rating/RatingControl.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import { Rating, RatingSize, } from "@fluentui/react/lib"; 
-import { useState, useEffect} from "react";
-import { initializeIcons} from '@fluentui/react/lib';
-import useInit from "./useInit"
+import { RatingBase, Rating, RatingSize, IRating,initializeIcons, IRatingProps } from "@fluentui/react"; 
+import { useState, useEffect, useRef} from "react";
+import {useConst} from "@uifabric/react-hooks";
+import usePrevious from "./usePrevious"
+
 
 
 export interface IProps {
@@ -14,67 +15,84 @@ export interface IProps {
 
 const RatingControl = (props:IProps): JSX.Element => {
 
-     //STATE VARIABLES
-     const [rating, setRating] = useState<number|undefined>(props.rating);
-     //const [isLoading, setIsLoading] = useState<boolean>(true);
+    //REF Object
+    const ratingRef = useRef<IRating>(null);
     
-
-
-    //EFFECT HOOKS
-    //-Initialization : will happen only once = like a contructor
-    // useEffect(() => {
-    //     initializeIcons();
-    //     console.log("useEffect - initialization");
-    //     setIsLoading(false);
-    // }, []); 
-
-    const init = useInit(() => {
+    //STATE VARIABLES
+    const [rating, setRating] = useState<number>(props.rating ?? 0);
+     
+    //PREVIOUS 
+    // Get the previous value (was passed into hook on last render)
+    const previousRating = usePrevious(rating);
+    
+    //Will run once on first render, like a constructor
+    useConst(() => {
         initializeIcons();
     });
 
-    //WHEN rating changes, 
-    //if value is different from the one received from PROPS
-    //Send value back to PCF
+    //EFFECT HOOKS
     useEffect(() => {
-        console.log("RatingControl - useEffect rating changed : " + rating);
+        //console.log("RatingControl - useEffect rating changed : " + rating);
+        
+        //if value is different from the one received from PROPS
         if(rating !== props.rating)
         {
-            console.log("=> props.onChange(" + rating + ")");
+            //Send value back to PCF
+            //console.log("=> props.onChange(" + rating + ")");
             props.onChange(rating);
         }
         
-    }, [rating]);  
+    }, [rating]);  //WHEN rating changes, 
 
     useEffect(() => {
-        console.log("RatingControl - useEffect props changed : " + props.rating);
+        //console.log("RatingControl - useEffect props changed : " + props.rating);
         if(rating !== props.rating)
         {
-            setRating(props.rating)
+            setRating(props.rating ?? 0)
         }
         
     }, [props.rating]);  
 
-
-    
     const onStarChange = (ev: React.FocusEvent<HTMLElement>, rating?: number): void => {
-        console.log("RatingControl - onStarChanged : " + rating)
-        setRating(rating);
+        //console.log("RatingControl - onStarChanged : " + rating)
+        //setRating(rating ?? 0); //=> use setter to update state variable
     };
 
+    const onClickEvent = (ev: React.MouseEvent<HTMLElement>): void => {
+        console.log("Clicked : ");
 
-    console.log("RatingControl - Rendering : init.loaded = " + init.loaded)
-    return <div>
-            {init.loaded &&   
-                <Rating
-                    min={0} 
-                    max={5}
-                    size={RatingSize.Large}
-                    rating={rating}
-                    onChange={onStarChange}
-                    allowZeroStars={true}
-                />
-            }
-        </div>; //empty while loading 
+        console.log("Previous: " + rating);
+        if(ratingRef.current !== null){
+            let current:RatingBase = ratingRef.current as RatingBase;
+            let newRating = current.state.rating as number;
+            console.log("New: " + newRating);
+
+            setRating(newRating === rating ? 0 : newRating);
+        }
+
+    };
+
+    
+
+    console.log("RatingControl - Rendering : rating = " + rating)
+    return (
+    
+        <Rating
+            componentRef={ratingRef}    
+            min={0} 
+            max={5}
+            size={RatingSize.Large}
+            rating={rating}
+            //onChange={onStarChange}
+            allowZeroStars={true}
+            onClick={onClickEvent}
+            
+        />
+    );
+
+            
+
+         
      
 }
 export default RatingControl;
