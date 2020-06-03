a8fcdbe2f491c80a09978c2e2d6a12fbc2596ee6
drivardxrm <38399134+drivardxrm@users.noreply.github.com>
2020-06-02 21:32:57 -0400

    added es-lint

--- Rating/RatingControl.tsx
+++ Rating/RatingControl.tsx
@@ -1,130 +1,118 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as React from "react";
-import { ITheme, createTheme, getTheme, RatingBase, Rating, RatingSize, IRating,initializeIcons } from "@fluentui/react"; 
-import { useState, useEffect, useRef, useMemo, useLayoutEffect} from "react";
-import {useConst} from "@uifabric/react-hooks";
-import MaskedInput from "./MaskedInput"
-
+import {
+   ITheme,
+   createTheme,
+   getTheme,
+   RatingBase,
+   Rating,
+   RatingSize,
+   IRating,
+   initializeIcons,
+} from "@fluentui/react";
+import { useState, useEffect, useRef, useMemo, useLayoutEffect } from "react";
+import { useConst } from "@uifabric/react-hooks";
+import MaskedInput from "./MaskedInput";
 
 export interface IProps {
-    
-    //properties
-    rating: number|undefined;
-    icon: string;
-    unselectedicon:string;
-    color: string;
-    maxvalue:number;
+   //properties
+   rating: number | undefined;
+   icon: string;
+   unselectedicon: string;
+   color: string;
+   maxvalue: number;
 
-    isReadonly:boolean,
-    isMasked:boolean,
+   isReadonly: boolean;
+   isMasked: boolean;
 
-    //return function
-    onChange: (rating:number|undefined) => void;
+   //return function
+   onChange: (rating: number | undefined) => void;
 }
 
+const RatingControl = (props: IProps): JSX.Element => {
+   //REF Object
+   const componentRef = useRef<IRating>(null);
 
-const RatingControl = (props:IProps): JSX.Element => {
+   //MEMO
+   const componentTheme = useMemo<ITheme>(() => {
+      console.log("useMemo : custom theme");
+      const customTheme: ITheme = createTheme(getTheme());
+      customTheme.palette.themeDark = props.color; //= hover
+      customTheme.palette.themePrimary = props.color; //= hover contour
+      customTheme.palette.neutralPrimary = props.color; // icon color
+      return customTheme;
+   }, [props.color]);
 
-    //REF Object
-    const componentRef = useRef<IRating>(null);
+   //CUSTOM HOOK provided by FluentUI team : https://github.com/microsoft/fluentui/tree/master/packages/react-hooks
+   //Will run function once on first render, like a constructor.
+   useConst(() => {
+      console.log("useConst : initilize icons");
+      initializeIcons();
+   });
 
-    //MEMO
-    const componentTheme = useMemo<ITheme>(()=>{
-        let customTheme: ITheme = createTheme(getTheme());
-        customTheme.palette.themeDark = props.color; //= hover
-        customTheme.palette.themePrimary = props.color; //= hover contour
-        customTheme.palette.neutralPrimary = props.color;   // icon color
-        return customTheme;
-    },[props.color]);
-    
-    //CUSTOM HOOK provided by FluentUI team : https://github.com/microsoft/fluentui/tree/master/packages/react-hooks
-    //Will run once on first render, like a constructor.    
-    useConst(() => {
-        initializeIcons();
-    });
-    
-    //STATE HOOKS
-    const [rating, setRating] = useState<number>(props.rating ?? 0);
+   //STATE HOOKS
+   const [rating, setRating] = useState<number>(props.rating ?? 0);
 
-    //EFFECT HOOKS - side effect after render
-    useEffect(() => {
+   //EFFECT HOOKS - side effect after render
+   useEffect(() => {
+      //if value is different from the one received from PROPS
+      //Send value back to caller (PCF)
+      if (rating !== props.rating) {
+         console.log("useEffect: => props.onChange(" + rating + ")");
+         props.onChange(rating);
+      }
+   }, [rating]); //WHEN rating changes,
 
-        //if value is different from the one received from PROPS
-        //Send value back to caller (PCF)
-        if(rating !== props.rating)
-        {
-            
-            console.log("RatingControl - useEffect: => props.onChange(" + rating + ")");
-            props.onChange(rating);
-        }
-        
-    }, [rating]);  //WHEN rating changes, 
- 
-    //LAYOUTEFFECT HOOKS - side effect before render
-    useLayoutEffect(() => {
-        
-        if(rating !== props.rating)
-        {
-            console.log("useLayoutEffect props.rating changed : " + props.rating);
-            setRating(props.rating ?? 0)
-        }
-        
-    }, [props.rating]); //Props are changed
+   //LAYOUTEFFECT HOOKS - side effect before final render
+   useLayoutEffect(() => {
+      if (rating !== props.rating) {
+         console.log("useLayoutEffect props.rating changed : " + props.rating);
+         setRating(props.rating ?? 0);
+      }
+   }, [props.rating]); //Props are changed
 
+   //EVENT Handlers
+   const onChangeEvent = (ev: React.FocusEvent<HTMLElement>, rating?: number): void => {
+      //console.log("RatingControl - onStarChanged : " + rating)
+      setRating(rating ?? 0); //=> use setter to update state variable
+   };
 
+   //Hack to put value at zero if the selected value is clicked again
+   const onClickEvent = (ev: React.MouseEvent<HTMLElement>): void => {
+      if (componentRef.current !== null) {
+         const current: RatingBase = componentRef.current as RatingBase;
+         const clickedRating = current.state.rating ?? 0;
 
-    //EVENT Handlers
-    const onStarChange = (ev: React.FocusEvent<HTMLElement>, rating?: number): void => {
-        //console.log("RatingControl - onStarChanged : " + rating)
-        setRating(rating ?? 0); //=> use setter to update state variable
-    };
+         console.log("CLICK : Previous value: " + rating + ", New value: " + clickedRating);
+         //If newrating is the same as rating, means that the selected item was clicked => Clear the value
+         //otherwise set to new value
+         const newRating = clickedRating === rating ? 0 : clickedRating;
 
-    //Hack to put value at zero if the selected value is clicked again
-    const onClickEvent = (ev: React.MouseEvent<HTMLElement>): void => {
-        console.log("Rating control Clicked : ");
+         setRating(newRating);
+      }
+   };
 
-        console.log("Previous value: " + rating);
-        if(componentRef.current !== null){
-            let current:RatingBase = componentRef.current as RatingBase;
-            let newRating = current.state.rating ?? 0; 
-            
-            console.log("New value: " + newRating);
-            //If newrating is the same as rating, means that the selected item was clicked => Clear the value
-            //otherwise set to new value
-            setRating(newRating === rating ? 0 : newRating);
-        }
-
-    };
-
-
-    if(props.isMasked){
-        return(
-            <MaskedInput/>
-        );
-    }
-    else{
-        console.log("RatingControl - Rendering : rating = " + rating)
-        return (
-        
-            <Rating
-                componentRef={componentRef} 
-                icon={props.icon}   
-                unselectedIcon={props.unselectedicon}
-                min={0} 
-                max={props.maxvalue}
-                size={RatingSize.Large}
-                rating={rating}            
-                allowZeroStars={true}
-                //onChange={onStarChange}
-                onClick={onClickEvent}
-                theme={componentTheme}
-                readOnly={props.isReadonly}
-                
-            />
-        );
-    }       
-     
-}
+   if (props.isMasked) {
+      return <MaskedInput />;
+   } else {
+      console.log("-->Component Rendering : rating = " + rating);
+      return (
+         <Rating
+            componentRef={componentRef}
+            icon={props.icon}
+            unselectedIcon={props.unselectedicon}
+            min={0}
+            max={props.maxvalue}
+            size={RatingSize.Large}
+            rating={rating}
+            allowZeroStars={true}
+            //onChange={onChangeEvent}
+            onClick={onClickEvent}
+            theme={componentTheme}
+            readOnly={props.isReadonly}
+         />
+      );
+   }
+};
 export default RatingControl;
-
-
-
--- Rating/index.ts
+++ Rating/index.ts
@@ -1,117 +1,108 @@
-import {IInputs, IOutputs} from "./generated/ManifestTypes";
-import * as react from 'react';
-import * as ReactDOM from 'react-dom';
-import RatingControl, {IProps} from "./RatingControl";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import * as react from "react";
+import * as ReactDOM from "react-dom";
+import RatingControl, { IProps } from "./RatingControl";
 
 export class Rating implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+   private _notifyOutputChanged: () => void;
+   private _container: HTMLDivElement;
 
-	private _notifyOutputChanged:() => void;
-	private _container: HTMLDivElement;	
+   private _selectedRating: number | undefined;
 
-	private _selectedRating:number|undefined;
+   private _props: IProps = {
+      rating: undefined,
+      icon: "",
+      unselectedicon: "",
+      color: "",
+      maxvalue: 0,
+      isMasked: false,
+      isReadonly: false,
 
-	private _props:IProps = 
-	{
-		rating:undefined,
-		icon:"",
-		unselectedicon:"",
-		color:"",
-		maxvalue: 0,
-		isMasked:false,
-		isReadonly:false,
-		
-		onChange: this.notifyChange.bind(this)
-	}
-	
-	
-	/**
-	 * Empty constructor.
-	 */
-	constructor()
-	{
+      onChange: this.notifyChange.bind(this),
+   };
 
-	}
+   /**
+    * Empty constructor.
+    */
+   // eslint-disable-next-line @typescript-eslint/no-empty-function
+   constructor() {}
 
-	/**
-	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-	 * Data-set values are not initialized here, use updateView.
-	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
-	 */
-	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
-	{
-		console.log("index - init");
-		// Add control initialization code
-		this._notifyOutputChanged = notifyOutputChanged;
-		this._container = document.createElement("div");
-		container.appendChild(this._container);
-	}
+   /**
+    * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+    * Data-set values are not initialized here, use updateView.
+    * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+    * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+    * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+    * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+    */
+   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+   public init(
+      context: ComponentFramework.Context<IInputs>,
+      notifyOutputChanged: () => void,
+      state: ComponentFramework.Dictionary,
+      container: HTMLDivElement,
+   ) {
+      console.log("index - init");
+      // Add control initialization code
+      this._notifyOutputChanged = notifyOutputChanged;
+      this._container = document.createElement("div");
+      container.appendChild(this._container);
+   }
 
+   /**
+    * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+    * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+    */
+   public updateView(context: ComponentFramework.Context<IInputs>): void {
+      // If the bound attribute is disabled because it is inactive or the user doesn't have access
+      let isReadOnly = context.mode.isControlDisabled;
 
-	/**
-	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-	 */
-	public updateView(context: ComponentFramework.Context<IInputs>): void
-	{
-		// If the bound attribute is disabled because it is inactive or the user doesn't have access
-		let isReadOnly = context.mode.isControlDisabled;
+      let isMasked = false;
+      // When a field has FLS enabled, the security property on the attribute parameter is set
+      if (context.parameters.ratingvalue.security) {
+         isReadOnly = isReadOnly || !context.parameters.ratingvalue.security.editable;
+         isMasked = !context.parameters.ratingvalue.security.readable;
+      }
 
-		let isMasked = false;
-		// When a field has FLS enabled, the security property on the attribute parameter is set
-		if (context.parameters.ratingvalue.security) {
-			isReadOnly = isReadOnly || !context.parameters.ratingvalue.security.editable;
-			isMasked =  !context.parameters.ratingvalue.security.readable
-		}
-		
-		// Add code to update control view
-		this._selectedRating = context.parameters.ratingvalue.raw || undefined;
-		
-		//Prepare the props to send to react component
-		this._props.rating = this._selectedRating;
-		this._props.icon = context.parameters.icon.raw || "";
-		this._props.unselectedicon = context.parameters.unselectedicon.raw || "";
-		this._props.color = context.parameters.color.raw || "";
-		this._props.maxvalue = context.parameters.maxvalue.raw ?? 0;
-		
-		console.log("index - updateView-> Render props.rating = " + this._props.rating);
-		ReactDOM.render(
-			react.createElement(RatingControl,this._props)
-			, this._container
-		);
-		
+      // Add code to update control view
+      this._selectedRating = context.parameters.ratingvalue.raw || undefined;
 
-	}
+      //Prepare the props to send to react component
+      this._props.rating = this._selectedRating;
+      this._props.icon = context.parameters.icon.raw || "";
+      this._props.unselectedicon = context.parameters.unselectedicon.raw || "";
+      this._props.color = context.parameters.color.raw || "";
+      this._props.maxvalue = context.parameters.maxvalue.raw ?? 0;
+      this._props.isReadonly = isReadOnly;
+      this._props.isMasked = isMasked;
 
-	private notifyChange(newRating:number|undefined)
-	{
-		//console.log("index - notifyChange : newrating = " + newRating);
-		this._selectedRating = newRating;
-		this._notifyOutputChanged();
-	}
+      console.log("index - updateView-> Render props.rating = " + this._props.rating);
+      ReactDOM.render(react.createElement(RatingControl, this._props), this._container);
+   }
 
+   private notifyChange(newRating: number | undefined) {
+      //Set a global variable with the received value from the React component
+      this._selectedRating = newRating;
+      this._notifyOutputChanged();
+   }
 
-	/** 
-	 * It is called by the framework prior to a control receiving new data. 
-	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
-	 */
-	public getOutputs(): IOutputs
-	{
-		//console.log("index - getOutputs() : ratingvalue = " + this._selectedRating);
-		return {
-			ratingvalue:this._selectedRating
-		};
-	}
+   /**
+    * It is called by the framework prior to a control receiving new data.
+    * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+    */
+   public getOutputs(): IOutputs {
+      //console.log("index - getOutputs() : ratingvalue = " + this._selectedRating);
+      return {
+         ratingvalue: this._selectedRating,
+      };
+   }
 
-	/** 
-	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-	 * i.e. cancelling any pending remote calls, removing listeners, etc.
-	 */
-	public destroy(): void
-	{
-		// Add code to cleanup control if necessary
-		ReactDOM.unmountComponentAtNode(this._container);
-	}
+   /**
+    * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+    * i.e. cancelling any pending remote calls, removing listeners, etc.
+    */
+   public destroy(): void {
+      // Add code to cleanup control if necessary
+      ReactDOM.unmountComponentAtNode(this._container);
+   }
 }
